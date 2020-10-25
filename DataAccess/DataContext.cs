55b78c4a2f101bde2d10947191f40e058980c9e8
Leonis Ocampo Nieves <leonis.ocampo@gmail.com>
2020-10-24 19:30:28 -0500

    Agregando la cadena de conexión de la base de datos

--- DataAccess/DataContext.cs
+++ DataAccess/DataContext.cs
@@ -5,6 +5,13 @@ namespace DataAccess
 {
     public class DataContext : DbContext
     {
+        // En el parámetro viene la cadena de conexión
+        // con base se sobrecarga el constructor de la clase heredada(DbContext)
+        public DataContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
         public DbSet<Clients>      Clients      { get; set; }
         public DbSet<Orders>       Orders       { get; set; }
         public DbSet<DetailOrders> DetailOrders { get; set; }
--- VueSPA/Startup.cs
+++ VueSPA/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,11 @@ namespace VueSPA
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Se registró DataContext como inyección de dependencia
+            services.AddDbContext<DataContext>(
+                opts => opts.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
